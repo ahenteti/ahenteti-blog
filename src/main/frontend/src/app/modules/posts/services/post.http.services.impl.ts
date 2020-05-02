013c1a41e5474d97629e8fb1e4643cb39c7ccf53
ahenteti <ahmad.henteti@gmail.com>
2020-05-02 23:34:45 +0200

    add HTTP requests handleErrors

--- src/main/frontend/src/app/modules/posts/services/post.http.services.impl.ts
+++ src/main/frontend/src/app/modules/posts/services/post.http.services.impl.ts
@@ -1,13 +1,17 @@
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
 import { Injectable } from "@angular/core";
 import { from } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, catchError } from "rxjs/operators";
 import {
   IPostApiResponse,
   IPostSummaryApiResponse,
 } from "../models/post.external.models";
 import { PostHttpServices } from "./post.http.services";
-import { PostsSummaries, IPost } from "../models/post.internal.models";
+import {
+  PostsSummaries,
+  IPost,
+  EmptyPost,
+} from "../models/post.internal.models";
 import { PostConverter } from "../converters/post.converter";
 import { HttpClient } from "@angular/common/http";
 
@@ -28,16 +32,27 @@ export class PostHttpServicesImpl extends PostHttpServices {
           );
           return data;
         })
-      );
+      )
+      .pipe(catchError(this.handleError("getPostSummaries", [])));
   }
 
   getPostById(postId: number): Observable<IPost> {
-    return this.http.get<IPostApiResponse>(`/api/posts/${postId}`).pipe(
-      map((post) => {
-        const postAfterTransformation = this.postConverter.fromPost(post);
-        postAfterTransformation.id = postId;
-        return postAfterTransformation;
-      })
-    );
+    return this.http
+      .get<IPostApiResponse>(`/api/posts/${postId}`)
+      .pipe(
+        map((post) => {
+          const postAfterTransformation = this.postConverter.fromPost(post);
+          postAfterTransformation.id = postId;
+          return postAfterTransformation;
+        })
+      )
+      .pipe(catchError(this.handleError("getPostById", new EmptyPost())));
+  }
+
+  private handleError<T>(operation = "operation", result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result);
+    };
   }
 }
